5eb13a0f5bd7bd8f8d7c036b587f1ff21b6d9487
Mon2 <invoicebinder@gmail.com>
2017-09-01 20:08:46 +1000

    fixed deployment script

--- invoicebinderhome/src/main/build/test/deployTEST.bat
+++ invoicebinderhome/src/main/build/test/deployTEST.bat
@@ -4,6 +4,7 @@ rem **** TEST DEPLOYMENT ****
 set ibhome="C:\\Users\\Manpreet\\GitHub\\invoicebinder\\invoicebinder"
 
 echo "Build Invoicebinder"
-start /wait mvn -f %ibhome% clean install -Ptest -Pcontext
+call mvn -f %ibhome% clean install -Ptest -Pcontext
+
 echo "Finished Building Invoicebinder"
 winscp /script=test-winscp.script
