1789fec2119e5aebd7342d96c6d1de55506278c5
Mon2 <invoicebinder@gmail.com>
2017-09-30 21:00:15 +1000

    refactor, parent pom

---
+++ gwtvisualization/lib/run_gwtvisualization_install.bat
@@ -0,0 +1,2 @@
+rem install gwt visualisation library
+mvn install:install-file -Dfile=./gwt-visualization.jar -DgroupId=com.google.gwt.google-apis -DartifactId=gwt-visualization -Dversion=1.1.2 -Dpackaging=jar
--- gwtvisualization/lib/run_gwtvisualization_install.sh
+++ gwtvisualization/lib/run_gwtvisualization_install.sh
@@ -1,2 +1,4 @@
-rem install gwt visualisation library
+#!/usr/bin/env sh
+!bin/sh
+
 mvn install:install-file -Dfile=gwt-visualization.jar -DgroupId=com.google.gwt.google-apis -DartifactId=gwt-visualization -Dversion=1.1.2 -Dpackaging=jar
